aa2020f5ab4dc8f906245c0650d096341113b720
morimorig3 <mori.nto@outlook.jp>
2021-08-24 22:46:16 +0900

    add author

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,11 @@ module.exports = {
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
+      colors: {
+        twitter: '#55acee',
+        github: '#24292e',
+        qiita: '#55c500',
+      },
       fontFamily: {
         righteous: ['Righteous', 'cursive'],
       },
