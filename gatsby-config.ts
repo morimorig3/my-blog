c9b136dabc93c44730e417fc2e13ba03217dc3ca
morimorig3 <mori.nto@outlook.jp>
2023-02-23 17:10:05 +0900

    feat:マークダウンから記事の出力

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -1,4 +1,4 @@
-import type { GatsbyConfig } from "gatsby"
+import type { GatsbyConfig } from "gatsby";
 
 const config: GatsbyConfig = {
   siteMetadata: {
@@ -9,7 +9,30 @@ const config: GatsbyConfig = {
   // If you use VSCode you can also use the GraphQL plugin
   // Learn more at: https://gatsby.dev/graphql-typegen
   graphqlTypegen: true,
-  plugins: [],
-}
+  plugins: [
+    `gatsby-plugin-sharp`,
+    `gatsby-transformer-sharp`,
+    {
+      resolve: `gatsby-transformer-remark`,
+      options: {
+        plugins: [
+          {
+            resolve: `gatsby-remark-images`,
+            options: {
+              maxWidth: 700,
+            },
+          },
+        ],
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: "contents",
+        path: "./src/contents/",
+      },
+    },
+  ],
+};
 
-export default config
+export default config;
---
+++ src/contents/2023-02-24_first-article/index.md
@@ -0,0 +1,11 @@
+---
+author: morimorig3
+title: 最初の記事
+keyVisual: "./key-visual.jpg"
+---
+
+# マークダウンの記事
+
+記事本文
+
+![画像サンプル](./image_01.jpg)
