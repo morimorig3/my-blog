766607a887852d1056a0ecece5ff68759f298c90
morimorig3 <mori.nto@outlook.jp>
2023-02-25 10:51:18 +0900

    feat:ルーティングベース実装

---
+++ gatsby-node.ts
@@ -0,0 +1,30 @@
+import { GatsbyNode } from "gatsby";
+import path from "path";
+
+export const createPages: GatsbyNode["createPages"] = async function ({
+  actions,
+  graphql,
+}) {
+  const { data } = await graphql<Queries.pagesQuery>(`
+    query pages {
+      allMarkdownRemark(sort: { frontmatter: { updatedAt: DESC } }) {
+        nodes {
+          frontmatter {
+            slug
+          }
+        }
+      }
+    }
+  `);
+  if (!data) throw new Error("データの取得に失敗しました");
+  data.allMarkdownRemark.nodes.forEach((node) => {
+    if (!node.frontmatter?.slug) throw new Error("slugが存在しません");
+    const slug = node.frontmatter.slug;
+
+    actions.createPage({
+      path: `/posts/${slug}`,
+      component: path.resolve(`./src/templates/post.tsx`),
+      context: { slug: slug },
+    });
+  });
+};
---
+++ src/templates/post.tsx
@@ -0,0 +1,13 @@
+import { Link } from "gatsby";
+import React from "react";
+
+const Post = () => {
+  return (
+    <main>
+      <Link to="/">Home</Link>
+      <h1>about page</h1>
+    </main>
+  );
+};
+
+export default Post;
