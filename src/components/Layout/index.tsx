bf50802c19fc27a62d7c4e0a8f7274d8c82a3184
morimorig3 <mori.nto@outlook.jp>
2023-03-11 12:06:13 +0900

    feat:グローバルスタイルとSassの使用テスト

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -1,13 +1,15 @@
 import type { ReactNode } from 'react';
 import React from 'react';
 
+import * as styles from './Layout.module.scss';
+
 interface Props {
   children: ReactNode;
 }
 
 export const Layout = ({ children }: Props) => {
   return (
-    <div style={{ margin: `0 auto`, maxWidth: 650, padding: `0 1rem` }}>
+    <div className={styles.wrapper}>
       <h1>morimorig3.com</h1>
       {children}
     </div>
