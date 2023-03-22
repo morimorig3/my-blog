dbe8b5ce4a7f7961487b33120d155b673e7d688f
morimorig3 <mori.nto@outlook.jp>
2023-03-22 09:18:09 +0900

    feat:detailsのスタイル定義

--- gatsby-browser.tsx
+++ gatsby-browser.tsx
@@ -1,5 +1,5 @@
 import './src/styles/reset.css';
 import './src/styles/global.scss';
 import './src/styles/prism.css';
-import './src/styles/markdown.css';
+import './src/styles/markdown.scss';
 import '@fontsource/zen-kaku-gothic-new';
